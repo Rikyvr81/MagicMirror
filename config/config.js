e8f0a92112de600d60786acef7a062cb75b4d92e
Rikyvr81 <rikyvr81@hotmail.com>
2026-08-17 23:05:40 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -156,10 +156,10 @@ let config = {
 					const calendarUrl =
 						'https://calendar.google.com/calendar/embed?src=it.italian%23holiday@group.v.calendar.google.com&color=%230B8043&ctz=Europe/Rome&mode=MONTH' +
 						`&dates=${startDate}%2F${endDate}` +
-						'&showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0' style='border:0' width:'450' height:'350' frameborder='0' scrolling='no''>
+						'&showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0'
 
 					return `
-						<div class="second-family-calendar-frame"><iframe src='${calendarUrl}'</iframe></div>",
+						<div class="second-family-calendar-frame"><iframe src='${calendarUrl}' style='border:0' width:'450' height:'350' frameborder='0' scrolling='no'</iframe></div>",
 					`;
 				})()
 			}
