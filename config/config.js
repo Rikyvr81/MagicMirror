a1eb5f592cdaf221bd46438e055663e994f181c7
Rikyvr81 <rikyvr81@hotmail.com>
2026-08-17 12:58:01 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,57 +1,79 @@
 let config = {
-	address: "0.0.0.0",	// Address to listen on, can be:
-							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-							// - another specific IPv4/6 to listen on a specific interface
-							// - "0.0.0.0", "::" to listen on any interface
-							// Default, when address config is left out or empty, is "localhost"
+	address: "0.0.0.0",
 	port: 10000,
-	basePath: "/",	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
-									// you must set the sub path here. basePath must end with a /
-	ipWhitelist: [],	// Set [] to allow all IP addresses
-									// or add a specific IPv4 of 192.168.1.5 :
-									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-									// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+	basePath: "/",
+	ipWhitelist: [],
 
-	useHttps: false,			// Support HTTPS or not, default "false" will use HTTP
-	httpsPrivateKey: "",	// HTTPS private key path, only require when useHttps is true
-	httpsCertificate: "",	// HTTPS Certificate path, only require when useHttps is true
+	useHttps: false,
+	httpsPrivateKey: "",
+	httpsCertificate: "",
 
 	language: "it",
-	locale: "it-IT",   
+	locale: "it-IT",
 
-	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"],
 	timeFormat: 24,
 	units: "metric",
 
 	modules: [
 		{
-			module: "alert",
+			module: "alert"
 		},
-		{
-			module: "updatenotification",
-			position: "top_bar"
-		},
+
 		{
 			module: "clock",
-			position: "top_left"
-		},
-		{
-			module: "helloworld",
-			position: "top_bar",
+			position: "top_left",
 			config: {
-				text: "<iframe src='https://calendar.google.com/calendar/embed?src=rikyvr81@gmail.com&color=%23039BE5&src=it.italian%23holiday@group.v.calendar.google.com&color=%230B8043&ctz=Europe/Rome' style='border:0' width='1000' height='700' frameborder='0' scrolling='no'></iframe>"
+				displaySeconds: true,
+				showDate: true,
+				dateFormat: "dddd, D MMMM YYYY"
 			}
 		},
+
+		{
+			module: "weather",
+			position: "top_right",
+			header: "Meteo",
+			config: {
+				weatherProvider: "openmeteo",
+				type: "current",
+				lat: 45.3526,
+				lon: 10.8445
+			}
+		},
+
+		{
+			module: "weather",
+			position: "bottom_right",
+			header: "Previsioni",
+			config: {
+				weatherProvider: "openmeteo",
+				type: "forecast",
+				lat: 45.3526,
+				lon: 10.8445,
+				maxNumberOfDays: 5
+			}
+		},
+
+		{
+			module: "helloworld",
+			position: "middle_center",
+			config: {
+				text: `
+					<iframe
+						src="https://calendar.google.com/calendar/embed?src=rikyvr81@gmail.com&color=%23039BE5&src=it.italian%23holiday@group.v.calendar.google.com&color=%230B8043&ctz=Europe/Rome"
+								},
+
 		{
 			module: "calendar",
-			position: "bottom_bar", // Lo posiziona in basso, sotto il tablone mensile
+			position: "bottom_left",
 			header: "📋 TO DO LIST",
 			config: {
 				language: "it",
 				timeFormat: "absolute",
-				maximumEntries: 5, 
+				maximumEntries: 5,
 				displaySymbol: true,
+				fade: false,
 				calendars: [
 					{
 						url: "https://calendar.google.com/calendar/ical/6bb24c0fb7ed698a938c7a81d953e53f82e361c0f061bf25daaff1616cb8fe2d%40group.calendar.google.com/private-0fd6a3516912f9807cdf5c47c1deccd0/basic.ics",
@@ -60,32 +82,7 @@ let config = {
 					}
 				]
 			}
-		},
-		{
-			module: "compliments",
-			position: "lower_third"
-		},
-		{
-			module: "weather",
-			position: "top_right",
-			config: {
-				weatherProvider: "openmeteo",
-				type: "current",
-				lat: 45.3526,
-				lon: 10.8445
-			}
-		},
-		{
-			module: "weather",
-			position: "top_right",
-			header: "Weather Forecast",
-			config: {
-				weatherProvider: "openmeteo",
-				type: "forecast",
-				lat: 45.3526,
-				lon: 10.8445
-			}
-		},
+		}
 	]
 };
 
